92daefd37b210f6b42c0d088ae4157e9af1fcc01
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-12-12 10:53:56 +0900

    デザインの修正

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><a href="/">トップ</a></li>
-          <li><a href="/portfolio">ポートフォリオ</a></li>
-          <li><a href="/about">ブログ</a></li>
-          <li><a href="/contact">連絡</a></li>
-        </ul>
+    <header className="bg-white text-black">
+      <nav aria-label="Main Navigation">
+      <ul className="flex justify-end space-x-8 p-4 pr-12 text-lg">
+      <li><a href="/" className="no-underline">トップ</a></li>
+      <li><a href="/portfolio" className="no-underline">ポートフォリオ</a></li>
+      <li><a href="/blog" className="no-underline">ブログ</a></li>
+      <li><a href="/contact" className="no-underline">連絡</a></li>
+      </ul>
       </nav>
     </header>
   );
